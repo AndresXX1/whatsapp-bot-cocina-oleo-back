460e9dd984f31523e62855dbd21405c7ede5edf5
AndresveraXX <andres_9_09@hotmail.com>
2024-09-28 20:28:47 -0300

    ultimo 28/9

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-// db.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-        
-        });
-        console.log('Conectado a MongoDB');
-    } catch (error) {
-        console.error('Error conectando a MongoDB:', error);
-        process.exit(1);
-    }
+  try {
+    console.log('Valor de MONGODB_URI:', process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log('Conectado a MongoDB');
+  } catch (error) {
+    console.error('Error conectando a MongoDB:', error);
+    console.error('Variables de entorno:', process.env);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
