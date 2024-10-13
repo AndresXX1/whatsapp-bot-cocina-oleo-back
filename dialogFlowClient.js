10c3c3b1552e0d1baee8e798fe065b76011ebdb5
AndresveraXX <andres_9_09@hotmail.com>
2024-10-12 22:08:59 -0300

    ssss 12/10

--- dialogFlowClient.js
+++ dialogFlowClient.js
@@ -5,33 +5,14 @@ const path = require('path');
 
 require('dotenv').config();
 
-// Decodificar las credenciales desde base64
-const credentialsJSON = Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON_BASE64, 'base64').toString('utf-8');
-
-let credentials;
-try {
-    credentials = JSON.parse(credentialsJSON);
-} catch (error) {
-    console.error('Error al parsear GOOGLE_APPLICATION_CREDENTIALS_JSON_BASE64:', error);
-    throw error;
-}
-
 // Configurar el cliente de Dialogflow
 const sessionClient = new dialogflow.SessionsClient({
-    credentials: {
-        client_email: credentials.client_email,
-        private_key: credentials.private_key,
-    },
-    projectId: process.env.DIALOGFLOW_PROJECT_ID,
+    keyFilename: path.join(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS),
 });
 
 const projectId = process.env.DIALOGFLOW_PROJECT_ID;
 
 const detectIntent = async (text, sessionId) => {
-    if (!text || typeof text !== 'string') {
-        throw new Error('El mensaje del usuario está vacío o no es una cadena.');
-    }
-
     const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 
     const request = {
@@ -39,7 +20,7 @@ const detectIntent = async (text, sessionId) => {
         queryInput: {
             text: {
                 text: text,
-                languageCode: 'es-ES', // Ajusta según tu necesidad
+                languageCode: 'es', // Considera usar 'es-ES' para español de España o 'es-MX' para México
             },
         },
     };
