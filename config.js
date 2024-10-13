f673bd3d84f860fcbd698a44e6ad8e4294706363
AndresveraXX <andres_9_09@hotmail.com>
2024-10-13 14:31:02 -0300

    aaa 13/10

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-// config.js
-require('dotenv').config();
+if (!process.env.DIALOGFLOW_PROJECT_ID) {
+    require('dotenv').config();
+}
 
 module.exports = {
     projectId: process.env.DIALOGFLOW_PROJECT_ID,
