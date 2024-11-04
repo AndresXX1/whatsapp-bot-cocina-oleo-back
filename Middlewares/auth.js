ddea4ef38035aec3d3f2b811ac6fc87be941bcc0
AndresveraXX <andres_9_09@hotmail.com>
2024-11-04 03:59:20 -0300

    s

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -5,12 +5,14 @@ const authMiddleware = (req, res, next) => {
     if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json({ message: 'No se proporcionó un token de autenticación válido.' });
     }
+    
     console.log('Middleware ejecutado, token válido.');
 
     const token = authHeader.split(' ')[1];
     try {
-        const token = jwt.sign({ userId: user._id }, 'secreto');
-        req.user = decoded;
+        // Cambiado a jwt.verify para verificar el token
+        const decoded = jwt.verify(token, 'secreto');
+        req.user = decoded; // Guardamos la información del usuario en req.user
         next();
     } catch (error) {
         res.status(401).json({ message: 'Token inválido o expirado.' });
